1a9ee04147852a4a99949353838ee88ef567640e
suryamaddila <67618085+suryamaddila@users.noreply.github.com>
2025-08-03 20:41:26 +0530

    Update server.js

--- Phone_tracker/backend/server.js
+++ Phone_tracker/backend/server.js
@@ -1,8 +1,12 @@
+app.get('/api/message', (req, res) => {
+  res.json({ msg: "Hello from backend!" });
+});
 
 const express = require('express');
 const router = express.Router();
 const { trackPhone } = require('../controllers/logic');
 
 router.post('/track', trackPhone);
+
 
 module.exports = router;
